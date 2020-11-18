05202c5067e36a219b82ca67e4db51cc4da254e6
acctwdxab <71532324+acctwdxab@users.noreply.github.com>
2020-11-18 01:22:31 -0800

    Add files via upload

---
+++ words_in_both.py
@@ -0,0 +1,16 @@
+# Dan Wu
+# 11/18/2020
+# Write a function that takes two strings as parameters and returns a set of the words contained in both strings.
+
+def words_in_both(s1, s2):
+ """Take two strings and return a set of words contained in both strings"""
+ common_one = s1.lower().split(" ")
+ common_two = s2.lower().split(" ")
+ result = []
+ for words in common_one:
+    if (words in common_two) and (words not in result):
+        result.append(words)
+ return result
+
+
+
